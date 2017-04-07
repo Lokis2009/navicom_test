0a92caefa7beee8acff8f35015d59cc347ec8e22
Stanislav Blokh <s.e.blokh@gmail.com>
2017-04-07 11:24:35 +0300

    шось працюе

--- js/common.js
+++ js/common.js
@@ -2,4 +2,16 @@ var app = angular.module('todoApp',[]);
 
 app.controller('mainCtrl', function($scope){
 	
+	$scope.todos = []
+	
+	$scope.addTodo = function(){
+		$scope.todos.push({text: $scope.newText, done: false});
+		$scope.newText = ''
+		//console.log(todos)
+	} 
+	
 });
+
+/*app.factory('todos', function(){
+	
+});*/
